9b8fd9c557e3696a36be16cf06a0cc5daa71f258
Juliya-web <abzalova.iulia@yandex.ru>
2024-01-13 03:52:53 +0500

    script hw5

--- script.js
+++ script.js
@@ -59,46 +59,143 @@
 
 //2.4 lesson
 //1
-let h = 0
-do {
-    console.log('Привет');
-    h++
-} while (h < 2);
+//let h = 0
+//do {
+//console.log('Привет');
+//h++
+//} while (h < 2);
 
 //2
-let i = 1;
-do {
-    console.log(i);
-    i++;
-} while (i < 6);
+//let i = 1;
+//do {
+//console.log(i);
+//i++;
+//} while (i < 6);
 
 //3
-let a = 7;
-do {
-    console.log(a);
-    a++;
-} while (a < 23 && a > 7);
+//let a = 7;
+//do {
+//console.log(a);
+//a++;
+//} while (a < 23 && a > 7);
 //4
-const obj = {
-    'Коля': '200',
-    'Вася': '300',
-    'Петя': '400'
-}
-for (let salary in obj) {
-    console.log(`${salary} - зарплата ${obj[salary]} долларов`);
-}
+//const obj = {
+//'Коля': '200',
+//'Вася': '300',
+//'Петя': '400'
+//}
+//for (let salary in obj) {
+//console.log(`${salary} - зарплата ${obj[salary]} долларов`);
+//}
 //5
-let n = 1000;
-let num = 0;
-while (n >= 50) {
-    n /= 2;
-    num++;
-}
-console.log(n);
-console.log(num);
+//let n = 1000;
+//let num = 0;
+//while (n >= 50) {
+//n /= 2;
+//num++;
+//}
+//console.log(n);
+//console.log(num);
 
 //6
 
-for (let dayNumber = 5; dayNumber <= 31; dayNumber += 7) {
-    console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+//for (let dayNumber = 5; dayNumber <= 31; dayNumber += 7) {
+//console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+//}
+
+//2.5 lesson
+
+//1
+function min(a, b) {
+    let minA = a < b;
+    if (minA) {
+        console.log(a);
+    } else {
+        console.log(b)
+    }
 }
+min(5, 5);
+
+//2
+let number = prompt('Введите любое число');
+function usernum(number) {
+    if (number % 2 === 0) {
+        return 'Число четное';
+    } else {
+        return 'Число не четное';
+    }
+}
+let result = usernum(number);
+console.log(result);
+
+//3.1
+function userSquare(num) {
+    console.log(num * num);
+}
+userSquare(6);
+
+//3.2
+function printSquare(number) {
+    return number * number;
+}
+let total = printSquare(8);
+console.log(total);
+
+//4
+let age = prompt('Сколько Вам лет?');
+function userAge(age) {
+    if (age < 0) {
+        return 'Вы ввели неправильное значение';
+    } else if (age > 0 && age < 12) {
+        return 'Привет, друг!';
+    } if (age >= 13) {
+        return 'Добро пожаловать!';
+    }
+}
+alert(userAge(age));
+
+//5
+function userCheck(a, b) {
+    if (isNaN(a) || isNaN(b)) {
+        return 'Одно или оба значения не являются числом';
+    } else {
+        return a * b;
+    }
+}
+console.log(userCheck('r', 5));
+
+//6
+
+let n = prompt('Введите число');
+function checkNum() {
+    if (isNaN(n)) {
+        return 'Переданный параметр не является числом';
+    } else {
+        return n * 3;
+    }
+}
+alert(checkNum());
+
+//7 
+function getRectangleArea() {
+    return this.radius * 3.14;
+}
+function getRectanglePerimeter() {
+    return this.radius * 2 * 3.14;
+}
+const circle1 = {
+    radius: 2,
+    getArea: getRectangleArea,
+    getPerimeter: getRectanglePerimeter,
+}
+const circle2 = {
+    radius: 4,
+    getArea: getRectangleArea,
+    getPerimeter: getRectanglePerimeter,
+}
+console.log(circle1.getArea());
+console.log(circle1.getPerimeter());
+console.log(circle2.getArea());
+console.log(circle2.getPerimeter());
+
+//8
