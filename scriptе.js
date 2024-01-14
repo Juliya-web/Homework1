1b9bb4021fe6f518771d7a49a9d862038d3336d4
Juliya-web <abzalova.iulia@yandex.ru>
2024-01-14 12:39:01 +0500

    доработка

--- scriptе.js
+++ scriptе.js
@@ -2,16 +2,16 @@
 function date() {
     let mounth = Number(prompt('Ввведите номер месяца'));
     if (mounth === 12 || mounth === 1 && mounth === 2) {
-        return 'Зима';
+        alert('Зима');
     } else if (mounth >= 3 && mounth <= 5) {
-        return 'Весна';
+        alert ('Весна');
     }
     else if (mounth >= 6 && mounth <= 8) {
-        return 'Лето';
+        alert ('Лето');
     }
     else if (mounth >= 9 && mounth <= 11) {
-        return 'Осень';
+        alert ('Осень');
     } else {
-        return 'Такого месяца не существует';
+        alert ('Такого месяца не существует');
     }
 }
